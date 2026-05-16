f4286c93a82b68d54b0cdaf619e55324213fb19a
Keightiie <35301742+Keightiie@users.noreply.github.com>
2026-05-16 23:59:46 +1000

    feat(blips): parse blip set config data

--- src/rolechat/audio/blip_config.cpp
+++ src/rolechat/audio/blip_config.cpp
@@ -1,7 +1,32 @@
 #include "blip_config.h"
+#include "rolechat/filesystem/RCDir.h"
+#include "rolechat/filesystem/RCFile.h"
+#include "rolechat/util/JsonUtils.h"
 
 BlipConfig::BlipConfig(const std::string& name) : m_name(name)
 {
+  rolechat::fs::RCDir blipDirectory("sounds/blips/" + m_name);
+  std::string configPath = blipDirectory.findFirst() + "/blip.json";
+  bool configExists = rolechat::fs::RCFile::exists(configPath);
+
+  rolechat::JsonUtils::JsonData jsonData = rolechat::JsonUtils::loadFile(configPath, m_validConfig);
+
+  if(!m_validConfig)
+    return;
+
+  m_sound = jsonData.value("sound", "default.wav");
+  m_allowBlank = jsonData.value("allow_blanks", false);
+
+  m_blipRate = jsonData.value("blip_rate", 30);
+
+  if(jsonData.contains("gender_overrides") && jsonData["gender_overrides"].is_object())
+  {
+
+    for (auto& [gender, sound] : jsonData["gender_overrides"].items())
+    {
+      m_genderOverrides[gender] = sound;
+    }
+  }
 }
 
 std::string BlipConfig::soundFile(const std::string &gender)
--- src/rolechat/audio/blip_config.h
+++ src/rolechat/audio/blip_config.h
@@ -12,11 +12,17 @@ public:
   std::string name() { return m_name; };
   std::string soundFile(const std::string& gender);
   int blipRate() { return m_blipRate; };
+  bool blanksAllowed() { return m_allowBlank; }
+
+  bool valid() { return m_validConfig; }
 
 private:
   std::string m_name;
   std::string m_sound;
   int m_blipRate = 0;
+  bool m_allowBlank = false;
+
+  bool m_validConfig = false;
 
   std::map<std::string, std::string> m_genderOverrides = {};
 };
