a2de2cee3283bfda3a806a4bc008f9befd07da99
Keightiie <35301742+Keightiie@users.noreply.github.com>
2026-08-21 23:20:21 +1000

    refactor: get rid of calls to removed function

--- src/rolechat/background/JsonBackgroundData.cpp
+++ src/rolechat/background/JsonBackgroundData.cpp
@@ -34,10 +34,6 @@ void JsonBackgroundData::loadBackground(const std::string &backgroundPath)
     searchVariantDirectory();
 
     setRandomizeState(j.value("randomize_position", false));
-    if(j.value("randomize_position", false))
-    {
-      randomizeDefault();
-    }
 
 }
 
